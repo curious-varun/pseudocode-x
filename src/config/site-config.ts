3df5af52c8a743ca6bbf2a0d8281a4b7368e60ab
Varun <devvarunbhardwaj@gmail.com>
2025-02-10 20:48:32 +0530

    installed shadcn

---
+++ src/config/site-config.ts
@@ -0,0 +1,7 @@
+import type { Metadata } from "next";
+
+export const siteConfig: Metadata = {
+  title: "pseudo-code",
+  description: "pseudo-code",
+  icons: [{ rel: "icon", url: "/favicon.ico" }]
+};
---
+++ src/providers.tsx
@@ -0,0 +1,10 @@
+"use client"
+import { ReactNode } from "react";
+export const Providers = ({ children }: { children: ReactNode }) => {
+
+  return (
+    <>
+      {children}
+    </>
+  );
+}
