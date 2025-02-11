d8cb04fbd10215452ebdb8c8de91137cc65cd656
Varun <devvarunbhardwaj@gmail.com>
2025-02-11 21:16:18 +0530

    :: buggy and shit

--- src/app/(problems)/problem/[slug]/page.tsx
+++ src/app/(problems)/problem/[slug]/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { Button } from "@/components/ui/button"; Button
 import {
   ResizableHandle,
   ResizablePanel,
@@ -10,7 +11,7 @@ import Editor from "@monaco-editor/react";
 
 
 
-
+//TODO: do we need to have a top bar here , or else use the context ? 
 export default function ProblemPage() {
   const [code, setCode] = useState("// Write your code here...");
   return (
@@ -27,8 +28,12 @@ export default function ProblemPage() {
         <ResizableHandle className="w-1 mx-0.5 hover:bg-blue-500 bg-white dark:bg-black dark:hover:bg-blue-600" withHandle />
         <ResizablePanel minSize={30} >
           <div className="mr-2  bg-accent/40 h-screen">
+            <Button onClick={() => alert(code)}>
+              submit
+            </Button>
             <Editor
-              height="400px"
+              height="h-screen"
+              className="h-screen"
               defaultLanguage="cpp"
               defaultValue={code}
               onChange={(value) => setCode(value || "")}
