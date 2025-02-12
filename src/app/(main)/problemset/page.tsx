097cdcfbae49c3bf92565ccb2c16ee9f5bd8356a
Varun <devvarunbhardwaj@gmail.com>
2025-02-12 22:48:05 +0530

    added problem set page

--- src/app/(main)/problemset/page.tsx
+++ src/app/(main)/problemset/page.tsx
@@ -1,10 +1,11 @@
+import { ProblemSetTable } from "@/components/problemset-table";
 import { getAllProblem, getAllProblemType } from "@/db/problem";
 
 export default async function ProblemsetPage() {
   const problems: getAllProblemType[] = await getAllProblem();
   return (
     <div>
-      instide probleset page page.tsx
+      <ProblemSetTable problems={problems} />
     </div>
   );
 } 
--- src/components/problemset-table.tsx
+++ src/components/problemset-table.tsx
@@ -1,10 +1,38 @@
+"use client";
 import { getAllProblemType } from "@/db/problem";
+import { ColumnDef } from "@tanstack/react-table";
+import { DataTable } from "@/components/data-table";
+export const problemColumns: ColumnDef<getAllProblemType>[] = [
+  {
+    accessorKey: "id",
+    header: () => <div className="ml-20">Problem ID</div>,
+    cell: ({ row }) => (
+      <p className="ml-20 text-sm font-medium">{row.original.id}</p>
+    ),
+  },
+  {
+    accessorKey: "title",
+    header: "Title",
+    enableColumnFilter: true,
+    accessorFn: (row) => row.title || "N/A",
+    cell: ({ row }) => <p className="text-sm font-medium">{row.original.title}</p>,
+  },
+  {
+    accessorKey: "dificulty",
+    header: "Difficulty",
+    cell: ({ row }) => <p className="text-sm font-medium">{row.original.dificulty}</p>,
+  },
+];
+
+
 
 export function ProblemSetTable({ problems }: { problems: getAllProblemType[] }) {
   return (
     <div>
-
+      <DataTable columns={problemColumns} data={problems} />
     </div>
   )
-
 }
+
+
+
