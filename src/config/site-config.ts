0c81263f99b636295f2a6010b89e134f5324c1af
Varun <devvarunbhardwaj@gmail.com>
2025-02-15 16:41:37 +0530

    we lost , and i learned

--- src/config/site-config.ts
+++ src/config/site-config.ts
@@ -3,5 +3,6 @@ import type { Metadata } from "next";
 export const siteConfig: Metadata = {
   title: "pseudo-code",
   description: "pseudo-code",
+  // i learnd tody 
   icons: [{ rel: "icon", url: "/favicon.ico" }]
 };
